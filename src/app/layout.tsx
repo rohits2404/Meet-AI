b1d710f635ca894336b2997b609676d4dd11044f
Rohit Sharma <rohits8731@gmail.com>
2026-03-11 00:33:49 +0530

    01: Setup

---
+++ src/app/layout.tsx
@@ -0,0 +1,33 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { cn } from "@/lib/utils";
+
+const inter = Inter({
+    subsets:['latin'],
+    variable:'--font-sans'
+});
+
+export const metadata: Metadata = {
+    title: "Meet-AI | AI Meeting Assistant",
+    description: "Meet-AI is an AI-powered meeting platform that enables video calls, automatic transcription, smart summaries, and an intelligent assistant to revisit and analyze conversations.",
+    icons: {
+        icon: "/images/logo.svg",
+    },
+};
+
+export default function RootLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <html lang="en" className={cn("font-sans", inter.variable)}>
+            <body
+            className={`${inter.className} antialiased`}
+            >
+                {children}
+            </body>
+        </html>
+    );
+}
