3d9fa362245dddf9847efc42839f35895a6bf489
Rohit Sharma <rohits8731@gmail.com>
2026-03-11 12:42:42 +0530

    11: Agents Form

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { cn } from "@/lib/utils";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { TRPCReactProvider } from "@/trpc/client";
+import { Toaster } from "@/components/ui/sonner";
 
 const inter = Inter({
     subsets:['latin'],
@@ -30,6 +31,7 @@ export default function RootLayout({
                 className={`${inter.className} antialiased`}
                 >
                     <TooltipProvider>
+                        <Toaster/>
                         {children}
                     </TooltipProvider>
                 </body>
--- src/modules/agents/server/procedures.ts
+++ src/modules/agents/server/procedures.ts
@@ -1,10 +1,24 @@
 import { db } from "@/db";
 import { agents } from "@/db/schema";
-import { createTRPCRouter, baseProcedure } from "@/trpc/init";
+import { createTRPCRouter, baseProcedure, protectedProcedure } from "@/trpc/init";
+import { eq } from "drizzle-orm";
+import { z } from "zod";
+import { agentsInsertSchema } from "../schema";
 
 export const agentsRouter = createTRPCRouter({
+    getOne: protectedProcedure.input(z.object({ id: z.string() })).query(async ({ input }) => {
+        const [existingAgent] = await db.select().from(agents).where(eq(agents.id, input.id))
+        return existingAgent;
+    }),
     getMany: baseProcedure.query(async () => {
         const data = await db.select().from(agents);
         return data;
+    }),
+    create: protectedProcedure.input(agentsInsertSchema).mutation(async ({ input, ctx }) => {
+        const [createdAgent] = await db.insert(agents).values({
+            ...input,
+            userId: ctx.auth.user.id,
+        }).returning();
+        return createdAgent;
     }),
 });
